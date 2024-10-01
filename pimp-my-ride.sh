8f329c5000fface0473768faa043e731debe2ca1
Auto <autonomoid@gmail.com>
2024-10-01 20:52:05 +0100

    Update pimp-my-ride.sh

--- pimp-my-ride.sh
+++ pimp-my-ride.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python3 python3-venv
+sudo apt-get install python3 python3-venv ibcapstone-dev
 python3 -m venv venv
 . ./venv/bin/activate
-pip install pwntools
+pip install pwntools capstone ropper
